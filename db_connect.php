1ee10d60a6b4be3c0a7d01432da0eede7a7e258a
Hari7Me <92789048+Hari7Me@users.noreply.github.com>
2022-10-23 21:22:46 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('127.0.0.1:49599','azure','6#vWHD_$','MYSQLCONNSTR_localdb')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('127.0.0.1:49599','azure','6#vWHD_$','localdb')or die("Could not connect to mysql".mysqli_error($con));
